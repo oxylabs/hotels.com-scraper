6fb78e30232cbd1a408a42bc1742431329e88138
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:07:41 +0200

    Adding code examples/hotels.com_golang.go

---
+++ code examples/hotels.com_golang.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	const Username = "YOUR_USERNAME"
+	const Password = "YOUR_PASSWORD"
+
+	payload := map[string]string{
+		"source": "universal",
+		"url": "https://www.hotels.com/hotel-search?regionid=84&latlong=53.412911%2c-8.24389&startdate=2023-12-27&d1=2023-12-27&enddate=2023-12-29&d2=2023-12-29&rooms=1&adults=2&amenities=ocean_view&destination=ireland",
+	}
+
+	jsonValue, _ := json.Marshal(payload)
+
+	client := &http.Client{}
+	request, _ := http.NewRequest("POST",
+		"https://realtime.oxylabs.io/v1/queries",
+		bytes.NewBuffer(jsonValue),
+	)
+
+	request.SetBasicAuth(Username, Password)
+	response, _ := client.Do(request)
+
+	responseText, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(responseText))
+}
